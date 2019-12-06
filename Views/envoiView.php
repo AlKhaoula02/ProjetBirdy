5c6f16e303e274c77688844b0329c5c00578751b
Khaoula <55798632+AlKhaoula02@users.noreply.github.com>
2019-12-06 16:30:42 +0100

    envoi

--- Views/envoiView.php
+++ Views/envoiView.php
@@ -9,17 +9,14 @@ include 'header.php';
 
         <div class="col-md-12">
 
-        <div class="formulaire">             
-                <span>Transférer : </span> 
-                <form>
+            <div class="formulaire">
+                <span>Transférer : </span>
+                <form method="post" action="index.php?page=envoi" enctype="multipart/form-data">
                     <label for="file" class="label-file"> un fichier</label>
-                    <input id="file" class="input-file" type="file" multiple="" name="fichier"><br/>
-                    <label for="file-dossier" class="label-file">un dossier</label>
-                    <input id="file-dossier" class="input-file" tabindex="-1" type="file" multiple="" webkitdirectory="webkitdirectory" directory="directory" name="dossier">
-                </form>        
-                <button type="submit" class="transfer__button transfer__button--inactive">Envoyer</button>
+                    <input id="file" class="input-file" type="file" multiple="multiple" name="fichier[]"><br />
+                    <button type="submit" class="transfer__button transfer__button--inactive" name="btn">Envoyer</button>
+                </form>
             </div>
- 
 
         </div>
 
--- Views/header.php
+++ Views/header.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="Assets/img/fav.ico"/>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" href="assets/img/fav.ico"/>
+    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
     <link href="https://fonts.googleapis.com/css?family=Gentium+Basic|Rosario&display=swap" rel="stylesheet">
     <title><?= $title ?></title>
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
@@ -16,5 +16,5 @@
 <body id= "body">
 
     <header id="header" class="">
-    <h1><img src="Assets/img/logo-birdy.png" alt="logo du site"></h1>
+    <h1><img src="assets/img/logo-birdy.png" alt="logo du site"></h1>
     </header>
