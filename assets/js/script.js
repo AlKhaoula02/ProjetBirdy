f7dcc21fd64c6c3e73b9a71257169cf66cf91e44
floriandreau <36734324+floriandreau@users.noreply.github.com>
2019-12-09 08:29:10 +0100

    moi

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+
+ //def de la variable qui recup le nom
+ let recup_file = document.getElementById("file");
+let affiche_list=document.getElementById("p");
+let list_files=document.getElementById("list_files");
+
+function reset() {
+    let p = document.getElementsByTagName('p');
+    console.log(p);
+    for (let index = 0; index < p.length; index++) {
+        const element = p[index];
+        
+        element.classList.add("hide");
+    }
+}
+
+document.getElementById('file').onchange=function(){
+    // On récupère la liste des fichiers
+    var listeFichiers = document.getElementById('file').files;
+    for ( var i = 0; i < listeFichiers.length; i ++ )
+    {
+        list_files.classList.remove("hide");
+        let p = document.createElement("p");
+        var unFichier = listeFichiers[i];
+        var message = unFichier.name;
+        p.innerHTML=message;    
+        affiche_list.appendChild(p); 
+        }
+    };
+
+
